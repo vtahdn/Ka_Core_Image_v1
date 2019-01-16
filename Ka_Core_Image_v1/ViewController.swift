769e2092bed30fd2c9cb4f51c8dfce143dc35bed
Viet Asc <vtahdn@gmail.com>
2019-01-16 16:56:22 +0700

    Initial Commit

---
+++ Ka_Core_Image_v1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ka_Core_Image_v1
+//
+//  Created by Viet Asc on 1/16/19.
+//  Copyright © 2019 Viet Asc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
